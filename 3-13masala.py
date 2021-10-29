4ccba3dcc62029fc7781b9d3dcadf95a2be1c2f8
zavakh1r <93262159+zavakh1r@users.noreply.github.com>
2021-10-29 15:29:54 +0300

    bu  men ishlagan masalalardan

---
+++ 3-13masala.py
@@ -0,0 +1,65 @@
+'''
+R= int(input("Radiusni kiriting:"))
+n=3.14
+3*(R**2)
+L=2*n*R
+S=n*(R**2)
+print(L)
+print(S)
+'''
+'''
+a= int(input("Sonni kiriting:"))
+b= int(input("Sonni kiriting:"))
+R=(a+b)/2
+print(R)
+'''
+
+'''
+a= int(input("Sonni kiriting:"))
+b= int(input("Sonni kiriting:"))
+print (sqrt(a*b))
+'''
+
+'''
+a= int(input("Sonni kiriting:"))
+b= int(input("Sonni kiriting:"))
+Y=a+b
+K=a*b
+Q=(a**2)
+Q=(b**2)
+print (Y)
+print (K)
+print (Q)
+print (Q)
+'''
+'''
+#11
+a= int(input("Sonni kiriting:"))
+b= int(input("Sonni kiriting:"))
+print (a+b)
+print (a*b)
+print ((a**2))
+print ((b**2))
+print (abs(-56))
+'''
+'''
+#12
+math.sqrt
+a= int(input("Katetlarni kiriting:"))
+b= int(input("Katetlarni kiriting:"))
+print ("Gipatenuzasi,math.sqrt, Perimetri")             
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
